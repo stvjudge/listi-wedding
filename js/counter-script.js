0bd7e7dee4bff39a8f1a91d5113b0a1be1aa3ef5
stvjudge <istvan.biro03@gmail.com>
2022-06-23 14:17:14 +0200

    updates

--- js/counter-script.js
+++ js/counter-script.js
@@ -16,17 +16,21 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
-  if (days< 10) {
-    days = "0" + days;}
+  if (days < 10) {
+    days = "0" + days;
+  }
 
   if (hours < 10) {
-  hours = "0" + hours;
+    hours = "0" + hours;
   }
 
   if (minutes < 10) {
-  minutes = "0" + minutes;
+    minutes = "0" + minutes;
   }
   
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
 
   // Output the result in an element with id="demo"
   document.getElementById("counter").innerHTML = "Már csak " + days + " nap, " + hours + ":" + minutes + ":" + seconds + " van hátra!";
