ef941ae0ccdac908005210e9bd9d703e87f964d1
stvjudge <istvan.biro03@gmail.com>
2022-07-05 00:20:35 +0200

    updated all

--- js/onscrollevent.js
+++ js/onscrollevent.js
@@ -19,21 +19,3 @@ function fixnavbar() {
     navbar.classList.remove("sticky");
   }
 }
-
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
